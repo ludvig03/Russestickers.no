b38e7b9e76f79b2eb778e5ea5f2562500f7fb2e7
Ludvig <ludvig.bergsaker@gmail.com>
2023-03-23 20:43:56 +0100

    la til scroll to view

--- src/App.js
+++ src/App.js
@@ -184,7 +184,7 @@ export default function App() {
         <img src="https://i.ibb.co/QCzKC3p/image0.jpg" className=" mx-auto"></img>
       </div>
       <div>
-        <p className="mt-8 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+        <p className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
           Tidligere bestilling
         </p>
         <p className="text-sm mr-4 text-gray-600">Her ser du et eksempel på en tidligere bestilling hos oss, på bildet ligger det 250 stickers i størrelse 6cm</p>
