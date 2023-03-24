8bab9d2a8c42d0c59fb0d063b2b502f8db1933eb
Ludvig <ludvig.bergsaker@gmail.com>
2023-03-24 08:50:19 +0100

    endret rekkefølge på komponenter

--- src/components/priskalkulator.js
+++ src/components/priskalkulator.js
@@ -18,7 +18,7 @@ export default function Priskalkulator() {
 
 
   return (
-    <div className="bg-white pt-8 sm:py-28">
+    <div className="bg-white pt-16 sm:py-28">
         <div className='mx-auto'>
             <div className='flex ml-6 mb-10'>
                 <h1 className="text-3xl text-[#473b2b] font-semibold">Priskalkulator</h1>
--- src/components/velgoss.js
+++ src/components/velgoss.js
@@ -31,7 +31,7 @@ const features = [
 export default function Velgoss() {
   return (
     <div className="bg-white pt-24 pb-8 sm:py-32" class="lesmer">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+      <div className="mx-auto max-w-7xl pb-8 px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
           <h2 className="text-base font-semibold leading-7 text-[#473b2b]">Du bestemmer</h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
